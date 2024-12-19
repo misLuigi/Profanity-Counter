f7cfc772ba493645ac42f302bb38c26b6de80f11
misLuigi <162457852+misLuigi@users.noreply.github.com>
2024-12-19 16:11:38 +0900

    Added interactivity and design on the project. Added a picture on the bottom for fun.

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
 let swearCount = document.getElementById("swear-count");
 console.log(swearCount);
+let entriesSec = document.getElementById("entries-section");
+console.log(entriesSec);
+
+let count = 0
+
+function increment() {
+  count ++;
+  swearCount.textContent = count;
+}
+
+let date = new Date();
+let latestDateString = date.toLocaleDateString('en-US',{month: 'long', year: 'numeric', day: 'numeric'});
+let latestTimeString = date.toLocaleTimeString('en-US', {hour: 'numeric', minute: 'numeric'});
+console.log(latestDateString);
+console.log(latestTimeString);
+
+function save() {
+  entriesSec.textContent += " - " + count;
+  swearCount.textContent = 0;
+  count = 0;
+}
